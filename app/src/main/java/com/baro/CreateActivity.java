f6363d3aca6b71efd54624b6af4cbe08f9561f27
James Rea <jamesrea97@gmail.com>
2021-03-14 23:56:59 +0100

    added skeleton of CreateActivity

--- app/src/main/java/com/baro/CreateActivity.java
+++ app/src/main/java/com/baro/CreateActivity.java
@@ -3,12 +3,59 @@ package com.baro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class CreateActivity extends AppCompatActivity {
+
+    private EditText courseEditText;
+    private ImageButton languageButton;
+    private ImageButton categoryButton;
+    private ImageButton createButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_activity);
+
+        configureCourseEditText();
+        configureCategoryButton();
+        configureLanguageButton();
+        configureCreateButton();
     }
+
+
+    private void configureCourseEditText() {
+        courseEditText = findViewById(R.id.edit_course_name);
+
+       // TODO Implement this method. Should change the courseName field as user types in
+    }
+
+    private void configureLanguageButton() {
+        languageButton = findViewById(R.id.btn_language);
+
+        languageButton.setOnClickListener(v -> {
+            // TODO Implement this method. Should somehow get users to select language
+        });
+    }
+
+
+    private void configureCategoryButton() {
+        categoryButton = findViewById(R.id.btn_category);
+
+        categoryButton.setOnClickListener(v -> {
+            // TODO Implement this method. Should somehow get users to select category - perhaps looking at the emojies on the keyboard?
+        });
+    }
+
+
+    private void configureCreateButton() {
+        createButton = findViewById(R.id.btn_create);
+
+        createButton.setOnClickListener(v -> {
+            // TODO Implement this method. Gathers the couresname, category, lannguage and creator UUID
+        });
+    }
+
+
 }
