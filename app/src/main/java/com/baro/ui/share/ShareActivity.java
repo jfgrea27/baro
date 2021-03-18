0590ac6bfbef64eb4ea82dc90402971f3970c2cd
jaker <jake.redrupp@gmail.com>
2021-03-18 15:26:28 +0000

    Added activity_share.xml, SendReceiveActivity.java and linked Local button

--- app/src/main/java/com/baro/ui/share/ShareActivity.java
+++ app/src/main/java/com/baro/ui/share/ShareActivity.java
@@ -1,7 +1,9 @@
 package com.baro.ui.share;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,9 +11,32 @@ import com.baro.R;
 
 public class ShareActivity extends AppCompatActivity {
 
+    private ImageButton internetButton;
+    private ImageButton localButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        configureInternetButton();
+        configureLocalButton();
+    }
+
+
+
+    private void configureInternetButton() {
+        internetButton = findViewById(R.id.btn_internet);
+        //TODO Setup Internet Button
+    }
+
+    private void configureLocalButton() {
+        localButton = findViewById(R.id.btn_local);
+
+        localButton.setOnClickListener(v -> {
+            Intent intentSendReceiveActivity = new Intent(ShareActivity.this,
+                    SendReceiveActivity.class);
+            startActivity(intentSendReceiveActivity);
+        });
     }
 }
