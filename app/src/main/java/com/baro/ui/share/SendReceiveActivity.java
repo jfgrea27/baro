ed060682408a73b03bf967ecaae88ec9fe6e9520
jaker <jake.redrupp@gmail.com>
2021-03-18 16:09:52 +0000

    Added activity_bluetooth_receive.xml, activity_bluetooth_send.xml, BluetoothReceiveActivity.java, BluetoothSendActivity.java, and linked buttons in UI

--- app/src/main/java/com/baro/ui/share/SendReceiveActivity.java
+++ app/src/main/java/com/baro/ui/share/SendReceiveActivity.java
@@ -2,15 +2,45 @@ package com.baro.ui.share;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import com.baro.R;
 
 public class SendReceiveActivity extends AppCompatActivity {
 
+    private ImageButton sendButton;
+    private ImageButton receiveButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_receive);
+
+        configureSendButton();
+        configureReceiveButton();
+    }
+
+
+
+    private void configureSendButton() {
+        sendButton = findViewById(R.id.btn_send);
+
+        sendButton.setOnClickListener(v -> {
+            Intent intentSendActivity = new Intent(SendReceiveActivity.this,
+                    BluetoothSendActivity.class);
+            startActivity(intentSendActivity);
+        });
+    }
+
+    private void configureReceiveButton() {
+        receiveButton = findViewById(R.id.btn_receive);
+
+        receiveButton.setOnClickListener(v -> {
+            Intent intentReceiveActivity = new Intent(SendReceiveActivity.this,
+                    BluetoothReceiveActivity.class);
+            startActivity(intentReceiveActivity);
+        });
     }
 }
