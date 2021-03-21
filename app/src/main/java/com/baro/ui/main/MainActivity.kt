e47c1ed2393235916366428349c80d6da3e4cb67
James Rea <jamesrea97@gmail.com>
2021-03-21 11:27:58 +0100

    added thumbnail picture in MainActivity

--- app/src/main/java/com/baro/ui/main/MainActivity.kt
+++ app/src/main/java/com/baro/ui/main/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.baro.ui.main
 
 import android.content.Intent
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
-import android.view.View
+import android.provider.MediaStore
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-
 import com.baro.AccountActivity
 import com.baro.R
+import com.baro.constants.FileEnum
+import com.baro.constants.JSONEnum
+import com.baro.helpers.FileHelper
+import com.baro.helpers.JSONHelper
+import com.baro.models.User
 import com.baro.ui.create.CreateActivity
 import com.baro.ui.learn.LearnActivity
 import com.baro.ui.share.ShareActivity
+import java.io.File
+import java.nio.file.Paths
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var accountButton: ImageButton
@@ -18,6 +32,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var createButton: ImageButton
     private lateinit var learnButton: ImageButton
 
+    private var user: User? = null
+    private var thumbnailUri: Uri? = null
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,6 +43,14 @@ class MainActivity : AppCompatActivity() {
         configureShareButton()
         configureCreateButton()
         configureLearnButton()
+
+        updateUserInfo()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun updateUserInfo() {
+        var userCredentialsRetrieve = UserCredentialsRetrieve()
+        userCredentialsRetrieve.execute()
     }
 
     private fun configureLearnButton() {
@@ -67,4 +93,63 @@ class MainActivity : AppCompatActivity() {
             startActivity(intentShareActivity)
         }
     }
+
+    private inner class UserCredentialsRetrieve : AsyncTask<Void?, Void?, Boolean?>() {
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        override fun doInBackground(vararg voids: Void?): Boolean? {
+            // Save the Meta information
+            user = retrieveUserCredentials()
+            // Save Photo URI
+            val uri = retrieveThumbnailUri()
+            if (uri != null) {
+                thumbnailUri = uri
+            }
+            return true
+        }
+
+        @RequiresApi(Build.VERSION_CODES.P)
+        override fun onPostExecute(result: Boolean?) {
+            if (result == true) {
+                if (thumbnailUri != null) {
+                    val source = ImageDecoder.createSource(contentResolver, thumbnailUri!!)
+                    val bitmap = ImageDecoder.decodeBitmap(source)
+                    accountButton.setImageBitmap(bitmap)
+                }
+            } else {
+                Toast.makeText(this@MainActivity, R.string.error_retrieving_credentials, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        private fun retrieveUserCredentials(): User {
+            val userMetaPath = Paths.get(
+                    getExternalFilesDir(null).toString(),
+                    FileEnum.USER_DIRECTORY.key,
+                    FileEnum.META_DATA_FILE.key)
+            val userMetaFile = File(userMetaPath.toString())
+            val content = FileHelper.readFile(userMetaFile)
+            val jsonObject = content?.let { JSONHelper.createJSONFromString(it) }
+
+            return  User(
+                    UUID.fromString(jsonObject?.get(JSONEnum.USER_UUID_KEY.key).toString()),
+                    jsonObject?.get(JSONEnum.USER_NAME_KEY.key).toString()
+            )
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        private fun retrieveThumbnailUri(): Uri? {
+            val userThumbnailPath = Paths.get(
+                    getExternalFilesDir(null).toString(),
+                    FileEnum.USER_DIRECTORY.key,
+                    FileEnum.PHOTO_THUMBNAIL_FILE.key)
+            val userThumbnailFile = File(userThumbnailPath.toString())
+            return if (userThumbnailFile.exists()) {
+                Uri.fromFile(userThumbnailFile)
+            } else {
+                null
+            }
+
+        }
+    }
+
 }
